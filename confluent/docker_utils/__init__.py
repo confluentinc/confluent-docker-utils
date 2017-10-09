44d63235ecb79062a24fe2f0dbcde27c1d9c5867
Alex Ayars <alex.ayars@confluent.io>
2017-10-09 14:39:18 -0700

    pass in tag as part of image name

--- confluent/docker_utils/__init__.py
+++ confluent/docker_utils/__init__.py
@@ -19,10 +19,10 @@ def build_image(image_name, dockerfile_dir):
     print(response)
 
 
-def image_exists(image_name, tag="latest"):
+def image_exists(image_name):
     client = docker.APIClient()
     tags = [t for image in client.images() for t in image['RepoTags']]
-    return "%s:%s" % (image_name, tag) in tags
+    return image_name in tags
 
 
 def pull_image(image_name):
