559f941305a40310e0b86cfd778d9e5dba5228d2
Alex Ayars <alex.ayars@confluent.io>
2017-12-18 14:27:23 -0800

    let add_registry_and_tag be scoped to UPSTREAM or TAG dependencies

--- test/test_docker_utils.py
+++ test/test_docker_utils.py
@@ -1,3 +1,30 @@
+from unittest.mock import patch
+
+import os
+
+import confluent.docker_utils as utils
+
+
 def test_imports():
     """ Basic sanity tests until we write some real tests """
     import confluent.docker_utils  # noqa
+
+
+def test_add_registry_and_tag():
+    """ Inject registry and tag values from environment """
+
+    base_image = "confluentinc/example"
+
+    fake_environ = {
+        "DOCKER_REGISTRY": "default-registry/",
+        "DOCKER_TAG": "default-tag",
+        "DOCKER_UPSTREAM_REGISTRY": "upstream-registry/",
+        "DOCKER_UPSTREAM_TAG": "upstream-tag",
+        "DOCKER_TEST_REGISTRY": "test-registry/",
+        "DOCKER_TEST_TAG": "test-tag",
+    }
+
+    with patch.dict('os.environ', fake_environ):
+        assert utils.add_registry_and_tag(base_image) == 'default-registry/confluentinc/example:default-tag'
+        assert utils.add_registry_and_tag(base_image, scope="UPSTREAM") == 'upstream-registry/confluentinc/example:upstream-tag'
+        assert utils.add_registry_and_tag(base_image, scope="TEST") == 'test-registry/confluentinc/example:test-tag'
