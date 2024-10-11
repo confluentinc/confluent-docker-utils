db93b4b033c6003067de280791ed39d3fb5f9c73
ConfluentSemaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2024-10-11 01:32:38 +0000

    [ci skip] Publish version 0.0.91

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='confluent-docker-utils',
-    version='0.0.90',
+    version='0.0.91',
 
     author="Confluent, Inc.",
     author_email="partner-support@confluent.io",
